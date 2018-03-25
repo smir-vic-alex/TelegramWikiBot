89cc2974b730fa9161efb8763eefd3debf7902d2
Smirnov-VA <smir.vic.alex@gmail.com>
2018-03-26 00:36:50 +0300

    bug fix: limit telegram callback text length, refactoring

--- business/src/main/java/com/business/delegates/SeeAlsoCollector.java
+++ business/src/main/java/com/business/delegates/SeeAlsoCollector.java
@@ -1,10 +1,12 @@
 package com.business.delegates;
 
 import com.business.BusinessConfig;
+import com.common.wiki.tgm.QuickException;
 import com.wiki.api.services.WikiApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.json.schema2pojo.dto.Link;
 import ru.json.schema2pojo.dto.Parse;
+import ru.json.schema2pojo.dto.Parse_;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,6 +17,10 @@ public class SeeAlsoCollector {
 
     private static final String SEE_ALSO_FIELD_KEY = "*";
     private static final String SEE_ALSO_FIELD = "see also";
+    private static final int TELEGRAM_CALL_BACK_TEXT_BUTTON_LIMIT = 64;
+    private static final String SEE_ALSO_LINKS_FOR_DOES_NOT_EXIST_MSG = "See Also links for does not exist";
+    private static final String SEE_ALSO_LINKS_FOR_PAGE_ID_MSG = "See Also links for pageId ";
+    private static final String DOES_NOT_EXIST_MSG = " does not exist";
 
     @Autowired
     private WikiApiService wikiApiService;
@@ -26,31 +32,55 @@ public class SeeAlsoCollector {
      * @return список ссылок
      */
     public List<String> getSeeAlsoLinks(Integer pageId) {
-        Integer seeAlsoSectionIndex = getSeeAlsoSectionIndex(pageId);
-        if (seeAlsoSectionIndex == null)
+        try {
+            return buildSeeAlsoList(pageId);
+        } catch (Throwable e) {
             return Collections.emptyList();
+        }
+    }
 
-        List<Link> linksList = wikiApiService.getPageLinkById(pageId, seeAlsoSectionIndex).getParse().getLinks();
-        return limitedSizeLinksList(linksList).stream()
+    private List<String> buildSeeAlsoList(Integer pageId) {
+        List<Link> linksList = getPageSeeAlsoLinks(pageId);
+
+        return linksList.stream()
                 .map(link -> link.getAdditionalProperties().get(SEE_ALSO_FIELD_KEY).toString())
+                .filter(link -> link.length() < TELEGRAM_CALL_BACK_TEXT_BUTTON_LIMIT)
+                .limit(BusinessConfig.getSeeAlsoListSize())
                 .collect(toList());
     }
 
-    private Integer getSeeAlsoSectionIndex(Integer pageId) {
-        Parse parse = wikiApiService.getPageSectionsById(pageId).getParse();
+    private List<Link> getPageSeeAlsoLinks(Integer pageId) {
+        Integer seeAlsoSectionIndex = findSeeAlsoSectionIndex(pageId);
+        Parse_ parse = wikiApiService.getPageLinkById(pageId, seeAlsoSectionIndex).getParse();
+        checkOnEmpty(parse, parse.getLinks());
 
-        if (parse != null && parse.getSections() != null) {
-            return parse.getSections().stream()
-                    .filter(section -> SEE_ALSO_FIELD.equals(section.getLine().toLowerCase()))
-                    .findFirst()
-                    .map(section -> Integer.parseInt(section.getIndex())).orElse(null);
-        }
-        return null;
+        return parse.getLinks();
     }
 
-    private List<Link> limitedSizeLinksList(List<Link> linksList) {
-        int defaultSize = BusinessConfig.getSeeAlsoListSize();
-        int toIndex = linksList.size() >= defaultSize ? defaultSize : linksList.size();
-        return linksList.subList(0, toIndex);
+    private Integer findSeeAlsoSectionIndex(Integer pageId) {
+        return getParse(pageId).getSections().stream()
+                .filter(section -> SEE_ALSO_FIELD.equals(section.getLine().toLowerCase()))
+                .findFirst()
+                .map(section -> Integer.parseInt(section.getIndex()))
+                .orElseThrow(() -> new SeeAlsoException(SEE_ALSO_LINKS_FOR_PAGE_ID_MSG + pageId + DOES_NOT_EXIST_MSG));
+    }
+
+    private Parse getParse(Integer pageId) {
+        Parse parse = wikiApiService.getPageSectionsById(pageId).getParse();
+        checkOnEmpty(parse, parse.getSections());
+
+        return parse;
+    }
+
+    private void checkOnEmpty(Object obj, List objList) {
+        if (obj == null || objList == null)
+            throw new SeeAlsoException(SEE_ALSO_LINKS_FOR_DOES_NOT_EXIST_MSG);
+    }
+
+    private class SeeAlsoException extends QuickException {
+
+        private SeeAlsoException(String message) {
+            super(message);
+        }
     }
 }
